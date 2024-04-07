565e5fe794b531e2670949b4f080bfc524be5c96
JRazek <jakub.razek2@gmail.com>
2024-04-07 13:00:12 +0200

    "checking

--- lagrangian.py
+++ lagrangian.py
@@ -4,24 +4,27 @@ import torch.nn.functional as F
 
 import numpy as np
 
-device = 'cuda'
+device = 'cpu'
 
 class LagrangianNN(nn.Module):
     def __init__(self):
         super(LagrangianNN, self).__init__()
-        self.fc1 = nn.Linear(2, 3)
-        self.fc2 = nn.Linear(3, 1)
+        self.fc1 = nn.Linear(2, 10)
+        self.fc2 = nn.Linear(10, 10)
+        self.fc3 = nn.Linear(10, 1)
         
     #takes x = (q, q_dot) as input
     #returns lagrangian of a system
     def forward(self, x):
-        f = F.sigmoid(self.fc1(x))
-        return F.sigmoid(self.fc2(f))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return F.sigmoid(x)
 
 def q_dot_dot(f, x):
-    grad = torch.autograd.grad(f(x), x, create_graph=True)
+    grad, = torch.autograd.grad(f(x), x, create_graph=True)
     hessian = torch.autograd.functional.hessian(f, x, create_graph=True).reshape(2, 2)
-    ddlddq_dot_dot = hessian[1, 1].reciprocal() * (grad[0][0][0] - hessian[1, 0] * x[0][1])
+    ddlddq_dot_dot = hessian[1, 1].reciprocal() * (grad[0] - hessian[1, 0] * x[1])
 
     return ddlddq_dot_dot
 
@@ -41,14 +44,14 @@ def l2_loss(q_dot_dot_predicted, q_dot_dot_actual):
 def generate_dataset():
     dataset = []
     targets = []
-    for t in range(100):
+    for t in range(0,100):
         q, q_dot, q_dot_dot = harmonic_osillator(t)
         dataset.append([q, q_dot])
         targets.append(q_dot_dot)
 
         
     dataset = torch.tensor(dataset, dtype=torch.float32, device=device, requires_grad=True)
-    targets = torch.tensor(targets, dtype=torch.float32, device=device, requires_grad=True)
+    targets = torch.tensor(targets, dtype=torch.float32, device=device)
     return dataset, targets
 
 
@@ -56,7 +59,7 @@ model = LagrangianNN().to(device)
 
 dataset, targets = generate_dataset()
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.1, weight_decay=1e-5)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 for epoch in range(1000):
     loss = torch.tensor([0], dtype=torch.float32, device=device, requires_grad=True)
@@ -64,10 +67,9 @@ for epoch in range(1000):
         optimizer.zero_grad()
         q_dot_dot_predicted = q_dot_dot(model, dataset[i])
         loss_i = l2_loss(q_dot_dot_predicted, targets[i])
-        loss = loss.add(loss_i)
+        loss_i.backward()
+        optimizer.step()
 
-    loss.backward()
-    optimizer.step()
     print('Epoch: ', epoch, 'Loss: ', loss.item())
 
 #q_dot_dot = q_dot_dot(lagrangian_function, dataset[0])
